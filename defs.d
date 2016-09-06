736c1d98ea6bf0e1c5b83b3075726a80bae31e8a
Zacharý <zacharyacutey@users.noreply.github.com>
2016-09-06 08:13:53 -0400

    Update and rename defs.d to enums.d

--- defs.d
+++
@@ -1,2 +0,0 @@
-module defs;
-import std.typecons;
---
+++ enums.d
@@ -0,0 +1,21 @@
+module enums;
+enum { //Color
+  NoColor,
+  White,
+  Black
+}
+enum { //Pieces
+  NoPiece,
+  WhitePawn,
+  WhiteRook,
+  WhiteKnight,
+  WhiteBishop,
+  WhiteQueen,
+  WhiteKing,
+  BlackPawn,
+  BlackRook,
+  BlackKnight,
+  BlackBishop,
+  BlackQueen,
+  BlackKing
+}
